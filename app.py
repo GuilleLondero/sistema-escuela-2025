b163e78e889b36153a23941dbbbad71fe69edd16
Guillermo Londero <guillelondero@gmail.com>
2025-06-07 23:49:59 -0300

    perfeccionado de app.py

--- app.py
+++ app.py
@@ -1,50 +1,28 @@
 #Importaciones:
-
-import sys # Importamos el módulo sys, que permite interactuar con el sistema y configurar el entorno de Python
-sys.tracebacklimit = 1 # Limitamos la cantidad de líneas que se muestran en el traceback para ver errores
-
-from fastapi import FastAPI #importamos FastAPI
-from routes.usuarios import usuario
-from routes.userdetail import userDetail
+import sys
+sys.tracebacklimit = 1
+from fastapi import FastAPI
+from routes.user import user
+from routes.career import career
+from routes.payment import payment
 from fastapi.middleware.cors import CORSMiddleware
 
-#Creación de la aplicación FastAPI:
-
+# Creamos la aplicación FastAPI:
 api_escu = FastAPI()
-api_escu.include_router(usuario)
-api_escu.include_router(userDetail)
 
+# Incorporamos las rutas importadas:
+api_escu.include_router(user)
+api_escu.include_router(career)
+api_escu.include_router(payment)
 
-#Creación del Middleware:
+# Agregamos middleware de CORS para permitir conexión desde frontend:
 api_escu.add_middleware(
-   CORSMiddleware,
-   allow_origins=["*"],
-   allow_credentials=True,
-   allow_methods=["GET", "POST", "PUT", "DELETE"],
-   allow_headers=["*"],
+    CORSMiddleware, # es el tipo de middleware a agregar
+    allow_origins=["*"], # Parámetro q permite acceso desde cualquier origen para conexión de FrontEnd (temporalmente)
+    allow_credentials=True, # Permitimos el uso de cookies,etc
+    allow_methods=["*"], # Permitimos todos los métodos HTTP
+    allow_headers=["*"], # Permitimos cualquier encabezado (ej: Authorization)
 )
 
 
-
-"""
-Importás FastAPI
-Creás una app
-Definís una o más rutas usando decoradores como @app.get() o @app.post()
-Respondes algo (texto, datos, etc.)
-
-#Definición de rutas (endpoints):
-@app.get("/")
-def helloword():
-    return "hello world"
-
-@app.get("/user")
-def mostrarInfoUsua():
-    return "“Hola Juancito! Has iniciado sesión con éxito!"
-
-@app.get("/users/{user_id}")
-def user():
-    return "soy un usuario!!"
-
-#Inicio del servidor Uvicorn:
-run(app, host="192.168.0.191", port=8000) 
-"""
+#uvicorn app:api_escu --reload

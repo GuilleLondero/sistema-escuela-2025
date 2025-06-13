962e11b8125413ffbd0714e8c5b0597928e5f977
Guillermo Londero <guillelondero@gmail.com>
2025-06-13 15:33:29 -0300

    modificaciones en modelo.py

--- models/modelo.py
+++ models/modelo.py
@@ -1,61 +1,120 @@
-from config.db import engine, Base
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import sessionmaker
+from configs.db import engine, Base
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
+from sqlalchemy.orm import sessionmaker, relationship
 from pydantic import BaseModel
+import datetime
 
-# region pydantic
-class Usuario(Base) :
-    __tablename__ = "usuarios"
+# region Modelos SQLAlchemy
+class User(Base):
+    __tablename__ = "user"
+    id = Column(Integer, primary_key =True)
+    username = Column( String(50), nullable=False, unique=True)
+    password = Column(String(40))
+    id_userdetail = Column(Integer, ForeignKey("userdetail.id"))
+    userdetail = relationship("UserDetail", uselist=False)
+    payments= relationship("Payment", uselist=True, back_populates="user")
+    pivoteusercareer = relationship("PivoteUserCareer", back_populates="user")
 
-    id = Column("id", Integer, primary_key =True)
-    username = Column("username", String(50), unique=True)
-    password = Column("pasword", String(50))
-
-    def __init__(self, username, password) :
+    def __init__(self, username, password):
         self.username = username
         self.password = password
 
-Base.metadata.drop_all(bind=engine)
+class UserDetail(Base):
+    __tablename__="userdetail"
+    id = Column(Integer, primary_key =True)
+    first_name = Column(String(50))
+    last_name = Column(String(50))
+    dni = Column(Integer)
+    type = Column(String(50))
+    email = Column(String(50), nullable=False, unique=True)
+
+    def __init__(self, first_name, last_name, dni, type, email):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.dni = dni
+        self.type = type
+        self.email = email
+
+class Career(Base):
+    __tablename__="career"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50))
+
+    def __init__(self, name):
+        self.name= name
+
+class Payment(Base):
+    __tablename__="payment"
+    id = Column(Integer, primary_key=True)
+    id_career=Column(Integer, ForeignKey("career.id"))
+    id_user=Column(Integer, ForeignKey("user.id"))
+    amount = Column(Integer)
+    affected_month = Column(DateTime)
+    created_at = Column(DateTime, default=datetime.datetime.now())
+    user = relationship("User", uselist=False, back_populates="payments")
+    career = relationship("Career", uselist=False)
+
+    def __init__(self, a, b, c, d):
+        self.id_career = a
+        self.id_user = b
+        self.amount = c
+        self.affected_month = d
+
+class PivoteUserCareer(Base):
+    __tablename__="pivote_user_career"
+    id = Column(Integer, primary_key=True)
+    id_career = Column(ForeignKey("career.id"))
+    id_user = Column(ForeignKey("user.id"))
+    career = relationship("Career", uselist=False)
+    user = relationship("User", uselist=False, back_populates="pivoteusercareer")
+
+    def __init__(self, id_user, id_career):
+        self.id_user = id_user
+        self.id_career = id_career
+# endregion
+
+# region Pydantic Models
+class InputUser(BaseModel):
+    username:str
+    password:str
+    firstname:str
+    lastname:str
+    dni:int
+    type:str
+    email:str
+
+class InputLogin(BaseModel):
+    username:str
+    password:str
+
+class InputUserDetail(BaseModel):
+    first_name:str
+    last_name:str
+    dni:int
+    type:str
+    email:str
+
+class InputCareer(BaseModel):
+    name: str
+
+class InputPayment(BaseModel):
+    id_career: int
+    id_user: int
+    amount: int
+    affected_month: datetime.date
+
+class InputUserAddCareer(BaseModel):
+    id_user: int
+    id_career: int
+# endregion
+
+# region configuraciones 
 Base.metadata.create_all(bind=engine)
 
 Session = sessionmaker(bind=engine)
 
 session = Session()
-
-class InputUsuario(BaseModel) :
-    id: int
-    username: str
-    password: str
-
 # endregion
-
-# region userdetails
-
-class UserDetail(Base):
-   __tablename__ = "userdetails"
-   id = Column("id", Integer, primary_key=True)
-   dni = Column("dni", Integer)
-   firstName = Column("firstName", String)
-   lastName = Column("lastName", String)
-   type = Column("type", String)
-   email = Column("email", String(80), nullable=False, unique=True)
-
-   def __init__(self, dni, firstName, lastName, type, email):
-       self.dni = dni
-       self.firstName = firstName
-       self.lastName = lastName
-       self.type = type
-       self.email = email
-
-class InputUserDetail(BaseModel):
-    dni: int
-    firstName: str
-    lastName: str
-    type: str
-    email: str
-
-# endregion   
-
 
 
 
